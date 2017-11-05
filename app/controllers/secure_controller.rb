8652ce4c9d2595aaeed6361453949cec67727fba
Andrew Wardrobe <andrew.g.wardrobe@googlemail.com>
2017-11-05 17:45:07 +0000

    Playing with rails admin

---
+++ app/controllers/secure_controller.rb
@@ -0,0 +1,3 @@
+class SecureController < ActionController::Base
+  before_action :authenticate_user!
+  end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -9,20 +9,6 @@ RailsAdmin.config do |config|
   end
   config.current_user_method(&:current_user)
 
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
-
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
 
   config.actions do
     dashboard                     # mandatory
@@ -38,5 +24,17 @@ RailsAdmin.config do |config|
     ## With an audit adapter, you can add:
     history_index
     history_show
+  end
+  config.model 'KruMember' do
+    list do
+      configure :bio do
+        label 'Biography'
+      end
+    end
+    edit do
+      configure :bio do
+        label 'Biography'
+      end
+    end
   end
 end
