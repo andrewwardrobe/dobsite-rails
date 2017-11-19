6c45879d49e163b8dd711207605d23836cc72caa
Andrew Wardrobe <andrew.g.wardrobe@googlemail.com>
2017-11-19 20:08:56 +0000

    mango kru layout

--- app/controllers/kru_members_controller.rb
+++ app/controllers/kru_members_controller.rb
@@ -50,7 +50,6 @@ class KruMembersController < ApplicationController
         format.html { redirect_to @kru_member, notice: 'Kru member was successfully created.' }
         format.json { render :show, status: :created, location: @kru_member }
       else
-        puts @kru_member.to_yaml
         format.html { render :new }
         format.json { render json: @kru_member.errors, status: :unprocessable_entity }
       end
